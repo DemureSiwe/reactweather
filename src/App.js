a97de58c4e27aa54f16faac65a7720f391b45649
DemureBusi <letsetse.b@gmail.com>
2025-12-16 01:13:17 +0200

    Deleted unnecessary files and added github link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from React
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Weather project</h1>
+
+      <footer>
+        Open sourced on Github by Busisiwe Tsetseng</footer>
+
     </div>
+
+    
+
   );
 }
 
-export default App;
